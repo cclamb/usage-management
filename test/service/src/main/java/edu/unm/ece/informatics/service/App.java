88c9dd57aa074f0113ca68b60a18a6720a86d3e3
cclamb <cclamb@cs.unm.edu>
2013-03-14 06:00:21 -0600

    initial commit

---
+++ test/service/src/main/java/edu/unm/ece/informatics/service/App.java
@@ -0,0 +1,13 @@
+package edu.unm.ece.informatics.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
